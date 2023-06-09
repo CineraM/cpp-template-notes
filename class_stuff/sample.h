7206a714d9f00311d8f18559d61a686f24c1af07
Matias Cinera <Matias.Cinera@qorvo.com>
2023-06-09 16:32:30 -0400

    Added namesapces & macro templates

--- class_stuff/sample.h
+++ class_stuff/sample.h
@@ -7,6 +7,20 @@
 
 enum mark_t {UNDISCOVERED, DISCOVERED, EXPLORED};
 
+namespace MathConstants
+{
+	const double kPI = 3.14159265;
+	const double eps0 = 8.85418781762039e-012;
+	const double micSq2mSq = 1e-12;
+	const double kRadToDegree = 180 / kPI;
+}
+
+namespace EnumNS
+{
+    enum PAMIMtrianglepresent { kNoTriangle, kUpperEdge,
+     kLowerEdge, kBothEdges };
+}
+
 class SampleClass
 {
     private:
@@ -21,3 +35,9 @@ class SampleClass
 };
 
 #endif
+
+/*
+unamed name spaces are only available within a file
+like static in C
+
+*/
--- class_stuff/sample_driver.cpp
+++ class_stuff/sample_driver.cpp
@@ -1,10 +1,27 @@
 
 #include "sample.h"
 
+#define AREA(l, b) (l * b)
+// definition of a macro
+
+using namespace EnumNS;
+using namespace MathConstants;
+//  exporting namespaces from header files
+
 int main()
 {
     SampleClass test_var = SampleClass(5);
     test_var.nums_getter();
+
+    int l1=20, l2=2, area;
+    area = AREA(l1,l2);
+
+    // std::cout << area << '\n'; // testing macros
+
+    std::cout << kPI << '\n';
+    std::cout << PAMIMtrianglepresent::kLowerEdge << '\n';
+    
+    
     return 1;
 }
 //  how to compile gcc -o rwmain	main.c	readerwriter.c	-l	pthread
